32333ab899a126d170a36c77cf504d40ec840532
Dhara Patel <dharacpatel9@gmail.com>
2024-02-15 10:54:12 -0500

    Initial Commit

---
+++ Meals/MealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealsApp.swift
+//  Meals
+//
+//  Created by Dharaa on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
