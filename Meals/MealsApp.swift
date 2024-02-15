1e4037c443544b963d10459c2b52cdb0bad32116
Dhara Patel <dharacpatel9@gmail.com>
2024-02-15 11:08:29 -0500

    Meals app: Fetch iOS Test

--- Meals/MealsApp.swift
+++ Meals/MealsApp.swift
@@ -2,16 +2,16 @@
 //  MealsApp.swift
 //  Meals
 //
-//  Created by Dharaa on 2/15/24.
+//  Created by Dharaa on 2/12/24.
 //
 
 import SwiftUI
 
 @main
-struct MealsApp: App {
+struct MealApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DessertListView()
         }
     }
 }
